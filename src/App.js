fd7aa4155cb5f75c3db2a03b70ed2c358024dd47
Patrick Araujo <rayssacoosta@gmail.com>
2021-04-05 23:44:32 -0300

    seções e ancoras

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import NavImg from '../src/img/flores.png';
 
 function App() {
   const [lightTheme, setLightTheme] = React.useState(true);
@@ -11,13 +12,47 @@ function App() {
   return (
     <div className={`App ${lightTheme ? ``:`dark-mode`}`} >
       <header className="App-header">
-        <h1>Portifolio</h1>
+        <a href="#sobremim"><img src={NavImg}/></a>
+        <nav className="App-nav">
+          <a href="#sobremim">Sobre mim</a>
+          <a href="#projetos">Projetos</a>
+          <a href="#formacoes">Formações</a>
+          <a href="#experiencia">Experiência</a>
+          <a href="#contato">Contato</a>
+        </nav>
       </header>
       <main className="App-main">
-        Rayssa
-        <button onClick={toggleTheme}>toggle</button>
+
+        <div id="sobremim" className="div-impar">
+          <h6>Bem vindo ao meu portifólio</h6>
+          <h1>Rayssa P. C. de Araujo</h1>
+          
+        </div>
+
+        <div id="projetos" className="div-par">
+           <h5>Em Construção</h5>
+       
+        </div>
+
+        <div id="formacoes" className="div-impar">
+           <h5>Em Construção</h5>
+       
+        </div>
+        
+        <div id="experiencia" className="div-par">
+          <h5>Em Construção</h5>
+
+        </div>
+
+        <div id="contato" className="div-impar">
+        <h5>Em Construção</h5>
+
+          <button onClick={toggleTheme}>Mudar tema</button>
+        </div>
       </main>
+
       <footer className="App-footer">
+        <p>© 2021 Rayssa Costa</p>
         <a href='https://br.freepik.com/vetores/flor'>Flor vetor criado por lukasdedi - br.freepik.com</a>
       </footer>
     </div>
